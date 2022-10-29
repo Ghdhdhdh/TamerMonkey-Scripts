97bd9558c7f97483ec703415297d875bcc71c993
Jacob_str <80145275+Ghdhdhdh@users.noreply.github.com>
2022-10-29 14:31:03 +1100

    Added blank youtube
    perfect for familys who dont allow youtube

---
+++ Blank Youtube.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Blank Youtube
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.youtube.com/
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.body.innerHTML = '<p>blocked</p>'
+    
+})();
